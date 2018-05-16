5e49233f9776d4382c4b0b612ef0bd52e6f9b93a
Mauricio <maujavier91@gmail.com>
2018-05-16 10:07:07 -0400

    Using Jquery to handle events and adding/removing classes

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,68 @@
-let red    = document.getElementById("red"),
-    green  = document.getElementById("green"),
-    yellow = document.getElementById("yellow"),
-    blue   = document.getElementById("blue");
+let red    = "red",
+    green  = "green",
+    yellow = "yellow",
+    blue   = "blue",
+    g="#"+green,
+    b="#"+blue,
+    r="#"+red,
+    y="#"+yellow,
+    gOn="greenOn",
+    bOn="blueOn",
+    rOn="redOn",
+    yOn="yellowOn";
+let sequence = []; 
 
-function pressedColor(){
-    alert(this.id);
+
+function toggleColor(colorOfSimon){
+    switch (this.id){
+        case green: $(g).toggleClass(gOn);
+        break;
+        case blue: $(b).toggleClass(bOn);
+        break;
+        case red: $(r).toggleClass(rOn);
+        break;
+        case yellow: $(y).toggleClass(yOn);
+        break;
+        default : 
+        $(g).removeClass(gOn);
+        $(b).removeClass(bOn);
+        $(r).removeClass(rOn);
+        $(y).removeClass(yOn);
+        break;
+    }
 }
 
-function lightUpColor(colorOfSimon) {
-    
-}
-    blue.addEventListener("click", pressedColor);
-    green.addEventListener("click", pressedColor);
-    yellow.addEventListener("click", pressedColor);
-    red.addEventListener("click", pressedColor);
-    blue.addEventListener("onmouseover",lightUpColor);
-    green.addEventListener("onmouseover",lightUpColor);
-    yellow.addEventListener("onmouseover",lightUpColor);
-    red.addEventListener("onmouseover",lightUpColor);
+function addToSequence(){
 
+}
+/** 
+ * This function takes a number coming from the sequence array and returns the string of the color 
+ * @param {number} elementOfSequence 
+ */
+function numberColor(elementOfSequence){
+
+    switch (elementOfSequence) {
+        case 0: return green;
+            break;
+        case 1: return red;
+            break;
+        case 2: return yellow;
+            break;
+        case 3: return blue;
+            break;
+        default:
+            break;
+    }
+}
+   $(r).mouseup(toggleColor);
+   $(b).mouseup(toggleColor);
+   $(g).mouseup(toggleColor);
+   $(y).mouseup(toggleColor);
+   $(r).mousedown(toggleColor);
+   $(b).mousedown(toggleColor);
+   $(g).mousedown(toggleColor);
+   $(y).mousedown(toggleColor);
+   $("body").mouseup(toggleColor);
+
+
+   
